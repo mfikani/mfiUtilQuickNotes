da127ef12a8e798f67219a7571903b15b9ec4dd3
miled.fikani <miled.fikani@sword-group.com>
2026-03-28 12:47:12 +0200

    First commit : files structure

---
+++ src/main/java/mfi/Util/QuickNotes/MfiUtilQuickNotes.java
@@ -0,0 +1,10 @@
+package mfi.Util.QuickNotes;
+
+public class MfiUtilQuickNotes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.	println("Hello World");
+	}
+
+}
